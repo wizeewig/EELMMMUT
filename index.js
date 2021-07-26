31078c22e01bdcecf015bcc2b4c7515f97f5e6f7
unknown <eel.webd.mmmut@gmail.com>
2021-07-26 19:52:59 +0530

    lisrct

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/linkedin', (req,res)=>{
-	res.redirect('https://www.linkedin.com/company/electrical-engineers-legation-mmmut');
+	res.redirect('https://www.linkedin.com/company/electrical-engineers-legation-mmmut','_blank');
 });
 app.get('/teams', (req,res)=>{
 	res.sendFile(__dirname + '/public/teams.html');
